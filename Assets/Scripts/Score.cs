773fd961f92d2a67fe85d5582c5f1c9906ebba54
Nehal Ahmad <nehalahmad15@gmail.com>
2021-02-06 16:30:38 +0530

    Update

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,36 @@
+﻿using Proyecto26;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public Text score;
+    public InputField UserEmail;
+
+    public static string uScore;
+    public static string uEmail;
+
+    public void SubmitDatabase()
+    {
+        uEmail = UserEmail.text;
+        uScore = score.text;
+
+        PostToDatabase();
+
+       
+    }
+
+    private void PostToDatabase()
+    {
+
+        User user = new User();
+        RestClient.Put("https://jigsawpuzzle-2ae60.firebaseio.com/" + uEmail + ".json", user);
+
+
+    }
+
+
+}
